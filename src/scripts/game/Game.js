af5926f7cfbebea787f464a329f76252affca350
Edleron <Edleron@hotmail.com>
2024-01-08 20:39:02 +0300

    add : platform

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,10 +1,21 @@
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
 import { Background } from "./Background";
+import { Platform } from "./Platform";
 
 export class Game extends Scene {
     create() {
         this.createBackground();
+        this.createPlatform({
+            rows: 4,
+            cols: 6,
+            x: 200
+        });
+    }
+
+    createPlatform(data) {
+        const platform = new Platform(data.rows, data.cols, data.x);
+        this.container.addChild(platform.container);
     }
 
     update(dt) {
@@ -15,4 +26,6 @@ export class Game extends Scene {
         this.bg = new Background();
         this.container.addChild(this.bg.container);
     }
+
+
 }
